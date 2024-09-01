c5fefd8233e98214b0b0df16f76fe05dbb9bb7ca
divyanshu310102 <yadavdivyanshu489@gmail.com>
2024-09-01 16:44:50 +0530

    added counterHandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  
+  let [counter,setCounter] = useState(15)
+
+
+  const incre = () =>{
+     counter += 1;
+     setCounter(counter)
+  }
+  const decre = () =>{
+    counter -= 1;
+    setCounter(counter)
+ }
+
+
   return (
     <>
-  <h1>Counter</h1>
+  <h1>Counter : {counter}</h1>
+  <button onClick={incre}>Increase</button>
+  <button onClick={decre}>Decrease</button>
   </>
   )
   
